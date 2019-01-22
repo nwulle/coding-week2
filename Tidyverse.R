e6079af46bd5510adda84077d8b2740b944adb13
nwulle <nicola.wullschleger@bluewin.ch>
2019-01-22 12:04:33 +0100

    new backup code

--- Tidyverse.R
+++ Tidyverse.R
@@ -97,11 +97,90 @@ rne %>%
   geom_bar() +
   scale_y_continuous(labels = scales::comma)
 
+library(hrbrthemes)
+
+cairo_pdf(file= "./plot1.pdf", width=7, height = 12)
 rne %>%
   count(`Libellé de la profession`, sort=TRUE) %>%
-  mutate(occupation= factor(`Libellé de la profession`)) %>% 
-  ggplot(aes(x=`cccupation`, y=n)) +
-  geom_bar(stat = "identity") +
+  filter(!is.na(`Libellé de la profession`)) %>% 
+  arrange(n) %>% 
+  filter(n>1000) %>% 
+  mutate(occupation = fct_inorder(`Libellé de la profession`)) %>%
+  mutate(coord=if_else(n>40000, n-1000, n+1000),
+         colour=if_else(n>40000, "white", "black")) %>% 
+  ggplot(aes(x=occupation, y=n)) +
+  geom_bar(stat = "identity", width = 0.6, fill="#391296") +
   scale_y_continuous(labels = scales::comma) +
-  coord_flip()
- 
+  coord_flip()+
+  geom_text(aes(label=occupation, y = coord, colour=colour), hjust="inward", vjust="center", size=2) +
+  scale_colour_manual(values=c("#391296", "white"), guide=FALSE)+
+  xlab("") +
+  ylab("Frequency") +
+  ylim(c(0,70000)) +
+  scale_x_discrete(labels=NULL) +
+  theme(axis.ticks.y=element_blank())+
+  theme_ipsum(grid="X")+
+  labs(title="Most elected officials are employees, farmers or retired", subtitle= "Number of elected officials in France in 2018 by occupation", caption="Source: RNE (Ministère de l'intérieur), computation by Sciences Po students")
+dev.off  
+
+
+#Small Multiplies
+
+cairo_pdf(file= "./plot1.pdf", width=12, height = 7)
+rne %>% 
+  mutate(gender = recode(`Code sexe`, "M" = "Male", "F" = "Female")) %>% 
+  count(`Libellé de la profession`,gender, sort = TRUE) %>% 
+  filter(!is.na(`Libellé de la profession`)) %>% 
+  ungroup %>% 
+  arrange(gender, n) %>% 
+  filter(n > 1000) %>% 
+  mutate(order = row_number()) %>% 
+  mutate(occupation = fct_inorder(`Libellé de la profession`)) %>% 
+  # creating new var coord so that if n past a certain limit, the label will be 
+  # inside the bar and white. otherwise will be black
+  mutate(coord = if_else(n > 22000, n - 1000, n + 1000),
+         colour = if_else(n > 22000, "white", "black")) %>% 
+  ggplot(aes(x = order, y = n)) +
+  geom_bar(aes(fill = gender), stat = "identity", width = 0.8) +
+  scale_fill_discrete( guide = FALSE) +
+  scale_y_continuous(labels = scales::comma) +
+  coord_flip() +
+  # geom_text() = creating new geometry of type 'text' onto plot
+  #     don't have to indicate x position as it was indicated in initial ggplot
+  #     have to change y position because we want labels 
+  #     y indicates the CENTER of the label 
+  #    hjust = horizontal justification 
+  geom_text(aes(label = occupation, y = coord, colour = colour), hjust = "inward", 
+            vjust = "center", size = 2) +
+  # set colours of text. remove legend
+  scale_color_manual(values = c("black", "white"), guide = FALSE) +
+  # facet wrap
+  facet_wrap(facets = vars(gender), scales = "free_y") +
+  # using xlab because coord_flip flipped x and y
+  # removing x axis label
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(labels = NULL) +
+  theme(axis.ticks.y = element_blank()) +
+  theme_ipsum(grid = "X") +
+  labs(title = "Most elected officials are employees, farmers or retired.", subtitle = "Number of elected officials in France in 2018 by occupation.", caption = "Source: RNE (Ministère de l'intérieur), computation by Sciences Po students.")
+dev.off()
+
+
+########################### MAPPING ############################################
+
+install.packages("sf")
+install.packages("mapview")
+install.packages("leaflet")
+install.packages("tmap")
+
+library(sf)
+library(mapview)
+library(leaflet)
+library(tmap)
+
+toilets<-read_sf("./data/sanisettesparis2011.geojson")
+toilets
+
+mapview(toilets)
+
